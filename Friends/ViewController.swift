7b8674f42b148ac33d636c728021f50d19ecf491
D_ttang <D_ttang@163.com>
2016-05-05 11:05:48 +0800

    Initial Commit

---
+++ Friends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by D_ttang on 16/5/5.
+//  Copyright © 2016年 D_ttang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
