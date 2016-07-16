fc22f91a614b400c0aebbeaf8e35fb0ae22c393b
D_ttang <D_ttang@163.com>
2016-07-16 13:04:08 +0800

    first commit

---
+++ Friends/index/FriendsLayout.swift
@@ -0,0 +1,13 @@
+//
+//  FriendsLayout.swift
+//  Friends
+//
+//  Created by D_ttang on 16/5/5.
+//  Copyright © 2016年 D_ttang. All rights reserved.
+//
+
+import UIKit
+
+class FriendsLayout: UICollectionViewFlowLayout {
+
+}
